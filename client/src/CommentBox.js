294d23a10b5664e3d1a144566985f81f57bc48c4
Seongsik Kim <seongsikkim2020@u.northwestern.edu>
2018-10-08 15:51:43 -0500

    can receive file from user

--- client/src/CommentBox.js
+++ client/src/CommentBox.js
@@ -180,11 +180,15 @@ class CommentBox extends Component {
   }
 
   uploadImage = () =>{
-    console.log("uploading image");
+    const { imagefile } = this.state;
   }
 
-  onChangeImage = () => {
-    console.log("image value changed");
+  onChangeImage = (selectorFiles) => {
+    const newState = { ...this.state };
+    // newState[e.target.imagefile] = e.target.value;
+    this.setState(newState);
+
+    console.log(selectorFiles.target.files);
   }
 
   render() {
@@ -217,6 +221,7 @@ class CommentBox extends Component {
           <ImageForm 
             uploadImage = {this.uploadImage}
             handleChangeImage = {this.onChangeImage}
+            imagefile = {this.imagefile}
           />
         </div>
         {this.state.error && <p>{this.state.error}</p>}
